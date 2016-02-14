49ff643dc325559ad1fa48d01549b757491be69b
Johann BATEAU <jbateau@e1r11p18.42.fr>
2016-02-14 19:38:14 +0100

    get_next_line retry

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbateau <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/02/04 14:29:59 by jbateau           #+#    #+#             */
+/*   Updated: 2016/02/14 19:33:41 by jbateau          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include "libft.h"
+# define BUFF_SIZE 16
+
+int		get_next_line(int const fd, char **line);
+
+#endif
